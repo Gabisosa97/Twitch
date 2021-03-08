4fe4ccd650dda6896e156a71769b0a1afa214cc6
Gabisosa97 <63225841+Gabisosa97@users.noreply.github.com>
2021-03-08 16:53:12 -0300

    Update chusma.php

--- chusma.php
+++ chusma.php
@@ -1,11 +1,11 @@
 <?php
-$r1 = "Sí"; 
-$r2 = "Claro que sí"; 
-$r3 = "No lo sé"; 
-$r4 = "Tal vez"; 
-$r5 = "Obviamente no"; 
-$r6 = "No."; 
-$respuestas = array($r1, $r2, $r3, $r4, $r5, $r6);
-$random = array_rand($respuestas); 
-echo $respuestas[$random];
+  $r1 = "Sí"; 
+  $r2 = "Claro que sí"; 
+  $r3 = "No lo sé"; 
+  $r4 = "Tal vez"; 
+  $r5 = "Obviamente no"; 
+  $r6 = "No."; 
+  $respuestas = array($r1, $r2, $r3, $r4, $r5, $r6);
+  $random = array_rand($respuestas); 
+  echo $respuestas[$random];
 ?>
